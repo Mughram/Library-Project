4a02975301a91dfb0ca1e25e8872a379c94cecda
Mughram <mogram.wrok@gmail.com>
2026-03-15 04:00:24 +0300

    Updated src

--- src/Member.java
+++ src/Member.java
@@ -2,7 +2,7 @@
 import java.util.*;
 public class Member extends Person {
     private List<LibraryItem> borrowedItems;
-    
+
     public Member(String id, String name) {
         super(id, name);
         this.borrowedItems = new ArrayList<>();
@@ -14,8 +14,12 @@ public class Member extends Person {
             item.setAvailable(false);
             System.out.println(getName() + " borrowed " + item.getTitle());
         }
+        else{
+            System.out.println(getName() + " trying to borrow " + item.getTitle());
+            System.out.println("Sorry this item not available right now");
+        }
     }
-    
+
     public void returnItem(LibraryItem item) {
         if (borrowedItems.remove(item)) {
             item.setAvailable(true);
